f91fd45f2eda21b4bd689948ec5a3d6d95d98a14
TommySanta <tomas.santamaria.benito@gmail.com>
2025-02-12 11:47:18 +0100

    Add project files.

---
+++ MvcCoreEF/Data/HospitalContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreEF.Models;
+
+namespace MvcCoreEF.Data
+{
+    public class HospitalContext: DbContext
+    {
+        public HospitalContext(DbContextOptions<HospitalContext> options):base(options) 
+        {
+        }
+        public DbSet<Hospital> Hospitales { get; set; }
+    }
+}
---
+++ MvcCoreEF/Repositories/RepositoryHospital.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreEF.Data;
+using MvcCoreEF.Models;
+
+namespace MvcCoreEF.Repositories
+{
+    public class RepositoryHospital
+    {
+        private HospitalContext context;
+        public RepositoryHospital(HospitalContext context)
+        {
+            this.context = context;
+        }
+        public async Task<List<Hospital>> GetHospitalesAsync()
+        {
+            var consulta = from datos in this.context.Hospitales
+                           select datos;
+            return await consulta.ToListAsync();
+        }
+
+        public async Task<Hospital> FindHospitalAsync(int idHospital)
+        {
+            var consulta = from datos in this.context.Hospitales
+                           where datos.IdHospital == idHospital
+                           select datos;
+            return await consulta.FirstOrDefaultAsync();
+        }
+        public async Task InsertHospitalAsync(int idHospital, string nombre, string direccion, string telefono, int camas)
+        {
+            Hospital hospital = new Hospital();
+            hospital.IdHospital = idHospital;
+            hospital.Nombre = nombre;
+            hospital.Direccion = direccion;
+            hospital.Telefono = telefono;
+            hospital.Camas = camas;
+
+            await this.context.Hospitales.AddAsync(hospital);
+            await this.context.SaveChangesAsync();
+        }
+        public async Task DeleteHospitalAsync(int idHospital)
+        {
+            Hospital hospital = await this.FindHospitalAsync(idHospital);
+
+            this.context.Hospitales.Remove(hospital);
+
+            await this.context.SaveChangesAsync();
+        }
+
+        public async Task UpdateHospitalAsync(int idHospital, string nombre, string direccion, string telefono, int camas)
+        {
+            Hospital hospital = await this.FindHospitalAsync(idHospital);
+            hospital.Nombre = nombre;
+            hospital.Direccion = direccion;
+            hospital.Telefono = telefono;
+            hospital.Camas = camas;
+
+            await this.context.SaveChangesAsync();
+
+        }
+    }
+}
